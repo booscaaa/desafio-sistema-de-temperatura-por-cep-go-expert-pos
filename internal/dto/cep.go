cb7892d61eb7227b0cf9a11dbfe7bcb898b36271
Vinícius Boscardin <boscardinvinicius@gmail.com>
2024-01-18 17:22:54 -0300

    :sparkles: feat: add dto layer

---
+++ internal/dto/cep.go
@@ -0,0 +1,15 @@
+package dto
+
+type CepOutput struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+	Erro        bool   `json:"erro"`
+}
---
+++ internal/dto/weather.go
@@ -0,0 +1,49 @@
+package dto
+
+type WeatherOutput struct {
+	Location Location `json:"location"`
+	Current  Current  `json:"current"`
+}
+
+type Current struct {
+	LastUpdatedEpoch int64     `json:"last_updated_epoch"`
+	LastUpdated      string    `json:"last_updated"`
+	TempC            float64   `json:"temp_c"`
+	TempF            float64   `json:"temp_f"`
+	IsDay            int64     `json:"is_day"`
+	Condition        Condition `json:"condition"`
+	WindMph          float64   `json:"wind_mph"`
+	WindKph          float64   `json:"wind_kph"`
+	WindDegree       int64     `json:"wind_degree"`
+	WindDir          string    `json:"wind_dir"`
+	PressureMB       float64   `json:"pressure_mb"`
+	PressureIn       float64   `json:"pressure_in"`
+	PrecipMm         float64   `json:"precip_mm"`
+	PrecipIn         float64   `json:"precip_in"`
+	Humidity         int64     `json:"humidity"`
+	Cloud            int64     `json:"cloud"`
+	FeelslikeC       float64   `json:"feelslike_c"`
+	FeelslikeF       float64   `json:"feelslike_f"`
+	VisKM            float64   `json:"vis_km"`
+	VisMiles         float64   `json:"vis_miles"`
+	Uv               float64   `json:"uv"`
+	GustMph          float64   `json:"gust_mph"`
+	GustKph          float64   `json:"gust_kph"`
+}
+
+type Condition struct {
+	Text string `json:"text"`
+	Icon string `json:"icon"`
+	Code int64  `json:"code"`
+}
+
+type Location struct {
+	Name           string  `json:"name"`
+	Region         string  `json:"region"`
+	Country        string  `json:"country"`
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	TzID           string  `json:"tz_id"`
+	LocaltimeEpoch int64   `json:"localtime_epoch"`
+	Localtime      string  `json:"localtime"`
+}
