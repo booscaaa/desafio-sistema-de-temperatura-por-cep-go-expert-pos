cfdb71bddbbdcd7c76e05e96fc969f1176e32f7c
bosca <boscardinvinicius@gmail.com>
2024-01-20 10:56:43 -0300

    :recycle: refactor(main.go): change env to json config

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,15 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
-
 	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-go-expert-pos/cmd/cmd"
 	"github.com/spf13/viper"
 )
 
 func init() {
-	viper.SetConfigFile(".env")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
-	if err := viper.MergeInConfig(); err != nil {
-		panic(fmt.Errorf("Erro ao ler o arquivo .env: %s", err))
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
 	}
 }
 
